00129a8f0ede6829decd0b1501a1a9b6353cf310
RPJunco <rodrigojunco023@gmail.com>
2024-07-06 21:06:52 -0300

    Se creo una carpeta 'js' el cual contiene un archivo llamado 'app.js'. Este archivo contendra el codigo necesario para administrar un arreglo de objetos

---
+++ js/app.js
@@ -0,0 +1,40 @@
+// Gestión de Inventario de Productos
+
+// Declaracion de la variable 'inventario' que contiene un arreglo de objetos
+let inventario = [
+    {
+        nombre: "Laptop",
+        categoria: "Electrónica",
+        precio: 1000,
+        cantidad: 10,
+        codigo: "P001"
+    },
+    {
+        nombre: "Smartphone",
+        categoria: "Electrónica",
+        precio: 700,
+        cantidad: 25,
+        codigo: "P002"
+    },
+    {
+        nombre: "Teclado",
+        categoria: "Accesorios",
+        precio: 50,
+        cantidad: 50,
+        codigo: "P003"
+    },
+    {
+        nombre: "Monitor",
+        categoria: "Electrónica",
+        precio: 300,
+        cantidad: 15,
+        codigo: "P004"
+    },
+    {
+        nombre: "Silla de Oficina",
+        categoria: "Muebles",
+        precio: 150,
+        cantidad: 20,
+        codigo: "P005"
+    }
+];
